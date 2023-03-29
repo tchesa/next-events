176ed715c1f36bf3f805ec591e8ff25bc73ec1ce
tchesa <cesarolimpio@gmail.com>
2023-03-29 09:07:40 -0300

    list all events in events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,12 @@
+import EventList from "@/components/events/event-list";
+import { getAllEvents } from "@/dummy-data";
+
 const EventsPage = () => {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>Events page</h1>
+      <EventList events={events} />
     </div>
   );
 };
