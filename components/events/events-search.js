3a78272e583e7ffeba1c689330668225bee1ef72
tchesa <cesarolimpio@gmail.com>
2023-03-29 09:22:44 -0300

    handle search

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -1,9 +1,18 @@
+import { useRef } from "react";
 import Button from "../ui/button";
 import classes from "./events-search.module.css";
 
-const EventsSearch = () => {
+const EventsSearch = ({ onSearch }) => {
+  const yearRef = useRef(null);
+  const monthRef = useRef(null);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const selectedYear = yearRef.current.value;
+    const selectedMonth = monthRef.current.value;
+
+    onSearch(selectedYear, selectedMonth);
   };
 
   return (
@@ -11,14 +20,14 @@ const EventsSearch = () => {
       <div className={classes.controls}>
         <div className={classes.control}>
           <label htmlFor="year">Year</label>
-          <select id="year">
+          <select id="year" ref={yearRef}>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
           </select>
         </div>
         <div className={classes.control}>
           <label htmlFor="month">Month</label>
-          <select id="month">
+          <select id="month" ref={monthRef}>
             <option value="1">January</option>
             <option value="2">February</option>
             <option value="3">Marth</option>
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,24 @@
 import EventList from "@/components/events/event-list";
 import EventsSearch from "@/components/events/events-search";
 import { getAllEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
 
 const EventsPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
+
+  const handleSearch = (year, month) => {
+    router.push({
+      pathname: "/events/[...slug]",
+      query: {
+        slug: [year, month],
+      },
+    });
+  };
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={handleSearch} />
       <EventList events={events} />
     </>
   );
