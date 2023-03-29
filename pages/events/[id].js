d904f75ed2bc0a01fb6f30ca92fa8fb3dc7c30e7
tchesa <cesarolimpio@gmail.com>
2023-03-29 08:54:30 -0300

    event detail page

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,14 +1,31 @@
+import EventContent from "@/components/events/event-content";
+import EventLogistics from "@/components/events/event-logistics";
+import EventSummary from "@/components/events/event-summary";
+import { getEventById } from "@/dummy-data";
 import { useRouter } from "next/router";
 
 const EventPage = () => {
   const router = useRouter();
-
   const eventId = router.query.id;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>Event not found</p>;
+  }
 
   return (
-    <div>
-      <h1>Event page</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   );
 };
 
